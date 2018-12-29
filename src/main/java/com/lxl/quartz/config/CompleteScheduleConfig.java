3f5c7667e3b223469311fcec9b12ed9de2596fa5
lixinlong <lxl19961016>
2018-12-29 15:30:08 +0800

    Initial commit

---
+++ src/main/java/com/lxl/quartz/config/CompleteScheduleConfig.java
@@ -0,0 +1,53 @@
+package com.lxl.quartz.config;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.util.StringUtils;
+import java.text.SimpleDateFormat;
+
+@Configuration
+@EnableScheduling
+public class CompleteScheduleConfig implements SchedulingConfigurer {
+
+
+
+    @Mapper
+    public interface CronMapper {
+        @Select("select cron from cron limit 1")
+        String getCron();
+    }
+
+    @Autowired
+    @SuppressWarnings("all")
+    CronMapper cronMapper;
+
+    /**
+     * 执行定时任务.
+     */
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addTriggerTask(
+                //1.添加任务内容(Runnable)
+                () -> System.err.println("执行定时任务2: " + SimpleDateFormat.getDateTimeInstance().format(System.currentTimeMillis())),
+                //2.设置执行周期(Trigger)
+                triggerContext -> {
+                    //2.1 从数据库获取执行周期
+                    String cron = cronMapper.getCron();
+                    //2.2 合法性校验.
+                    if (StringUtils.isEmpty(cron)) {
+                        // Omitted Code ..
+                        System.out.println("时间表达式为空");
+                    }
+                    //2.3 返回执行周期(Date)
+                    return new CronTrigger(cron).nextExecutionTime(triggerContext);
+                }
+        );
+    }
+
+}
